05ae37ba9a3fe188162ef0a9f956debfd1002ac3
aman <avinashthakurchauhan07@gmail.com>
2024-04-14 10:35:53 +0530

    initial commit --use mantine with tailwind css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import "@/styles/globals.css";
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+import '../styles/globals.css';
+
+
+// import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
